b84910babb6124cd7e3c56862c32274df8ded543
teodorro <edem.teo@gmail.com>
2020-09-17 20:52:06 +0300

    task

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,45 @@
+const val val75000 = 75000_00;
+const val val20 = 20_00;
+const val val006 = 0.006;
+const val val0075 = 0.0075;
+const val val35 = 35_00;
+
+
+fun main() {
+    calcAndPrint("Mastercard", 50000_00, 1000_00)
+    calcAndPrint("Mastercard", 150000_00, 1000_00)
+    calcAndPrint("Maestro", 50000_00, 1000_00)
+    calcAndPrint("Maestro", 150000_00, 1000_00)
+    calcAndPrint("Visa", 50000_00, 100_00)
+    calcAndPrint("Visa", 50000_00, 10000_00)
+    calcAndPrint("Мир", 50000_00, 100_00)
+    calcAndPrint("Мир", 50000_00, 10000_00)
+    calcAndPrint("vk", 50000_00, 1000_00)
+    calcAndPrint("vk", 50000_00, 1000_00)
+}
+
+fun calcAndPrint(cardType: String, thisMonthTransfers: Int, transferSize: Int) {
+    val afterCommission = calcAfterCommission(cardType, thisMonthTransfers, transferSize);
+    println(
+        "Карточка: $cardType\n"
+                + "Переводы за месяц: $thisMonthTransfers коп\n"
+                + "Размер переводимой суммы: $transferSize коп\n"
+                + "После комиссии: $afterCommission коп\n"
+    );
+}
+
+fun calcAfterCommission(cardType: String, thisMonthTransfers: Int, transferSize: Int): Int {
+    return when {
+        cardType == "Mastercard" || cardType == "Maestro" -> calcMastercardMaestro(thisMonthTransfers, transferSize)
+        cardType == "Visa" || cardType == "Мир" -> calcVisaPeace(transferSize)
+        else -> transferSize
+    }
+}
+
+fun calcVisaPeace(transferSize: Int): Int {
+    return if (transferSize * val0075 < val35) transferSize - val35 else (transferSize * (1 - val0075)).toInt();
+}
+
+fun calcMastercardMaestro(thisMonthTransfers: Int, transferSize: Int): Int {
+    return if (thisMonthTransfers <= val75000) transferSize else (transferSize * (1 - val006) - val20).toInt();
+}
